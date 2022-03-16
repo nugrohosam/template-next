106a1d88d88fe2277d197a284af52f632398f86c
Imanullah <pengguna21@gmail.com>
2022-03-16 12:33:14 +0700

    fix | filter status budget item group

--- src/modules/budgetPlanItemGroup/constant.ts
+++ src/modules/budgetPlanItemGroup/constant.ts
@@ -13,22 +13,12 @@ export enum BudgetPlanItemGroupStatus {
 }
 
 export const BudgetPlanItemGroupStatusOptions = [
-  { label: 'SUBMIT', value: BudgetPlanItemGroupStatus.Submit },
   { label: 'DRAFT', value: BudgetPlanItemGroupStatus.Draft },
-  { label: 'APPROVE', value: BudgetPlanItemGroupStatus.Approve },
   {
     label: 'WAITING APPROVAL',
     value: BudgetPlanItemGroupStatus.WaitingApproval,
   },
-  {
-    label: 'WAITING APPROVAL By HO',
-    value: BudgetPlanItemGroupStatus.WaitingApprovalByHo,
-  },
-  { label: 'CANCEL', value: BudgetPlanItemGroupStatus.Cancel },
-  { label: 'DELETE', value: BudgetPlanItemGroupStatus.Delete },
-  { label: 'FINAL', value: BudgetPlanItemGroupStatus.Final },
   { label: 'REJECT', value: BudgetPlanItemGroupStatus.Reject },
-  { label: 'REVISE', value: BudgetPlanItemGroupStatus.Revise },
   {
     label: 'WAITING APPROVAL PIC ASSET HO',
     value: BudgetPlanItemGroupStatus.WaitingApprovalPicAssetHo,
